3018b7199357b837e0e53f270ccb48e022616e8f
msmithstar <msmith@icstars.org>
2025-08-19 12:43:26 -0500

    Set up contact form to receive email responses and linked SM

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -26,9 +26,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          to_name: 'Maddy Smith',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'your-email@gmail.com', // Replace with your actual email
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,15 +8,30 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
-        <div className="social-icon">
+        <a 
+          href="https://github.com/msmithstar" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-icon"
+        >
           <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
+        </a>
+        <a 
+          href="https://instagram.com/maddoggyz" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-icon"
+        >
           <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        </a>
+        <a 
+          href="https://www.linkedin.com/in/maddy-smith-op/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-icon"
+        >
+          <img src="/assets/linkedin.png" alt="linkedin" className="w-15 h-15" />
+        </a>
       </div>
 
       <p className="text-white-500">© 2025 Maddy Smith. All rights reserved.</p>
